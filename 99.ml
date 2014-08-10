c11d4d3ba200204ed8d258d4d73ea59446aaff55
Jose Angel Riarola <txangel@acm.org>
2014-08-10 22:56:02 +0100

    Problem 1...
    (I need to find how to use a testing framework)

--- 99.ml
+++ 99.ml
@@ -1,1 +1,10 @@
 print_string "99 Ocaml Problems!\n";;
+
+
+print_string "(1) Write a function last : 'a list -> 'a option that returns the last element of a list.\n";;
+let rec last = function
+	| [] 		-> None
+	| [x] 		-> Some x
+	| _ :: tail -> last tail;;
+
+last["first", "second"];;
